464147c0d84d8662fed084f37d3b09e9cc8c61ff
Pratham2994 <prathampanchal02994@gmail.com>
2024-10-30 21:24:45 +0530

    a

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import { Button } from './components/ui/button'
-import {PopoverDemo} from './_compo/PopOver'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <PopoverDemo />
-  )
+    <>
+    </>
+  );
 }
 
 export default App
